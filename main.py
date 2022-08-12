208a52ed55e64d1a7cb9930c884f152425e30527
Andrii Terets <terets.andrey@gmail.com>
2022-08-12 15:35:58 +0300

    added flask, base.html template and index.html, examples.html, about.html scratches

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('hello')
+from flask import Flask, render_template, request, g, flash, abort, redirect, url_for
+
+app = Flask(__name__)
+
+menu = [{'name': 'NeuroStyle', 'url': '/'},
+        {'name': 'Examples', 'url': '/examples'},
+        {'name': 'About', 'url': '/about'}]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', menu=menu)
+
+
+@app.route('/examples')
+def examples():
+    return render_template('examples.html', menu=menu)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', menu=menu)
+
+if __name__ == '__main__':
+    app.run(debug=True)
