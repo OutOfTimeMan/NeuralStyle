988c506e9bd0532481e422a7ce29ca58f4f11a69
Andrii Terets <terets.andrey@gmail.com>
2022-08-16 19:55:13 +0300

    Started to create styles for login and register forms

--- forms.py
+++ forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField, SubmitField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
-    email = StringField('Email: ', validators=[Email("Некорректный email")])
-    psw = PasswordField('Пароль:', validators=[DataRequired(), Length(min=4, max=100, message='Пароль должен быть от 4 до 100 символов')])
-    remember = BooleanField('Запомнить', default=False)
-    submit = SubmitField('Войти')
+    email = StringField('Email ', validators=[Email("Некорректный email")])
+    psw = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=100, message='Пароль должен быть от 4 до 100 символов')])
+    remember = BooleanField('Remember me', default=False)
+    submit = SubmitField('Sign in')
 
 
 class RegisterForm(FlaskForm):
     email = StringField('Email: ', validators=[Email("Некорректный email")])
-    psw = PasswordField('Пароль:', validators=[DataRequired(), Length(min=4, max=100, message='Пароль должен быть от 4 от 100 символов')])
-    psw2 = PasswordField('Повтор пароля: ', validators=[DataRequired(), EqualTo('psw', message='Пароли не совпадают')])
-    submit = SubmitField('Регистрация')
+    psw = PasswordField('Password:', validators=[DataRequired(), Length(min=4, max=100, message='Пароль должен быть от 4 от 100 символов')])
+    psw2 = PasswordField('Repeat password: ', validators=[DataRequired(), EqualTo('psw', message='Пароли не совпадают')])
+    submit = SubmitField('Sign up')
