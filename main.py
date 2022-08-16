63269232b1943c2ac3a46987cb29c832a7e14d57
k-shakhray <shakhray3@gmail.com>
2022-08-16 16:36:21 +0300

    Added login and log out

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ import os
 from flask import Flask, render_template, request, g, flash, abort, redirect, url_for, make_response
 from NDataBase import NDataBase
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
+from UserLogin import UserLogin
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'enkjrgn3e45rt0342hf23kjfn2ekwlfn23wo4t'
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    print('load_user')
+    return UserLogin().fromDB(user_id, dbase)
 
 
 menu = [{'name': 'NeuroStyle', 'url': '/'},
@@ -17,12 +28,14 @@ menu = [{'name': 'NeuroStyle', 'url': '/'},
 DATABASE = '/tmp/neurostyle.db'
 DEBUG = True
 
-app.config.update(dict(DATABASE=os.path.join(app.root_path,'neurostyle.db')))
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'neurostyle.db')))
+
 
 def connect_db():
     conn = sqlite3.connect(app.config["DATABASE"])
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def create_db():
     db = connect_db()
@@ -31,17 +44,22 @@ def create_db():
     db.commit()
     db.close()
 
+
 def get_db():
     if not hasattr(g, 'link_db'):
         g.link_db = connect_db()
     return g.link_db
+
 
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'link_db'):
         g.link_db.close()
 
+
 dbase = None
+
+
 @app.before_request
 def before_request():
     global dbase
@@ -50,23 +68,41 @@ def before_request():
 
 
 '''ROUTES'''
+
+
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html', menu=menu)
+
 
 @app.route('/examples')
 def examples():
     return render_template('examples.html', menu=menu)
 
-@app.route('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        user = dbase.getUserByEmail(request.form['email'])
+        if user and check_password_hash(user['psw'], request.form['psw']):
+            userlogin = UserLogin().create(user)
+            rm = True if request.form.get('remainme') else False
+            login_user(userlogin, remember=rm)
+            return redirect(url_for('index'))
+
+        flash('Неверный логин или пароль', "error")
+
     return render_template('login.html', menu=menu)
+
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == "POST":
         if len(request.form['name']) > 1 and len(request.form['email']) > 4 \
-            and len(request.form['psw']) > 4 and request.form['psw'] == request.form['psw2']:
+                and len(request.form['psw']) > 4 and request.form['psw'] == request.form['psw2']:
             hash = generate_password_hash(request.form['psw'])
             res = dbase.addUser(request.form['name'], request.form['email'], hash)
             if res:
@@ -79,9 +115,15 @@ def register():
 
     return render_template("register.html", menu=menu, title="Регистрация")
 
+
 @app.route('/about')
 def about():
     return render_template('about.html', menu=menu)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
